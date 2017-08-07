95e32a067574dcffb9941eab65ceb31dfc4b72e1
stephengeller <stephen.geller@hotmail.com>
2017-08-07 15:32:36 +0100

    addded top_up method to Oystercard

--- oyster-puyan/lib/oystercard.rb
+++ oyster-puyan/lib/oystercard.rb
@@ -6,4 +6,9 @@ DEFAULT_BALANCE = 0
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
+
+  def top_up(amount)
+    @balance += amount
+    @balance
+  end
 end
--- oyster-puyan/spec/oystercard_spec.rb
+++ oyster-puyan/spec/oystercard_spec.rb
@@ -3,6 +3,11 @@ require 'oystercard'
 describe Oystercard do
   it 'Default balance of zero' do
     new_card = Oystercard.new
-    expect(new_card.balance).to eq(0)
+    expect(new_card.balance).to eq 0
+  end
+
+  it 'can be topped up' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
   end
 end
