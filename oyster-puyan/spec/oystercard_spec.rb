a6dd8635c5cc6790c181a69ba3e8c1a4138e94e3
pyan83 <puyanwei@gmail.com>
2017-08-07 13:27:20 +0100

    added tests and oystercard class

--- oyster-puyan/spec/oystercard_spec.rb
+++ oyster-puyan/spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
-describe OysterCard do
+require 'oystercard'
 
+describe Oystercard do
+  it 'Default balance of zero' do
+    new_card = Oystercard.new
+    expect(new_card.balance).to eq(0)
+  end
 end
